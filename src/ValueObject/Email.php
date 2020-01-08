95ca8838a6c81dc8f82193f72c9ffbbd93ead51e
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2020-01-08 13:43:03 +0100

    Add Assert and remove .lock

---
+++ src/ValueObject/Email.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+/**
+ * @author Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 08/01/2020
+ */
+
+namespace JeckelLab\AdvancedTypes\ValueObject;
+
+use Assert\Assert;
+
+/**
+ * Class Email
+ * @package App\Domain\ValueObject
+ */
+class Email
+{
+    /** @var string */
+    protected $value;
+
+    /**
+     * Email constructor.
+     * @param string $value
+     */
+    public function __construct(string $value)
+    {
+        Assert::that($value)->email();
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
