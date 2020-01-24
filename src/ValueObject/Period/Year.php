5f69d204477281f74b51b8fc51c4c95bcda93ce1
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2020-01-24 12:04:28 +0100

    Add year period

---
+++ src/ValueObject/Period/Year.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @author Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 24/01/2020
+ */
+
+declare(strict_types=1);
+
+namespace JeckelLab\AdvancedTypes\ValueObject\Period;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+
+/**
+ * Class Year
+ * @package JeckelLab\AdvancedTypes\ValueObject\Period
+ */
+class Year implements PeriodInterface
+{
+    /** @var int */
+    protected $year;
+
+    /** @var DateTimeImmutable */
+    protected $start;
+
+    /** @var DateTimeImmutable */
+    protected $end;
+
+    /**
+     * Year constructor.
+     * @param int $year
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function __construct(int $year)
+    {
+        $this->year = $year;
+
+        $this->start = DateTimeImmutable::createFromFormat(
+            'Y-m-d H:i:s',
+            sprintf('%d-01-01 00:00:00', $year)
+        );
+        $this->end = $this->start->modify('+1 year')->modify('-1 second');
+    }
+
+    /**
+     * @return int
+     */
+    public function year(): int
+    {
+        return $this->year;
+    }
+
+    /**
+     * @return DateTimeImmutable
+     */
+    public function start(): DateTimeImmutable
+    {
+        return $this->start;
+    }
+
+    /**
+     * @return DateTimeImmutable
+     */
+    public function end(): DateTimeImmutable
+    {
+        return $this->end;
+    }
+
+    /**
+     * @param DateTimeInterface $dateTime
+     * @return static
+     */
+    public static function byDateTime(DateTimeInterface $dateTime): self
+    {
+        return new self(
+            (int) $dateTime->format('Y')
+        );
+    }
+}
