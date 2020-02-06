50dc351ce993c8e0df5b67e02251c9afd801de8e
Julien Mercier-Rojas <devci@j3ck3l.me>
2020-02-06 10:58:09 +0100

    Add month factory by string

--- tests/ValueObject/Period/MonthTest.php
+++ tests/ValueObject/Period/MonthTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\JeckelLab\AdvancedTypes\ValueObject\Period;
 
+use JeckelLab\AdvancedTypes\ValueObject\Exception\InvalidArgumentException;
 use JeckelLab\AdvancedTypes\ValueObject\Period\Month;
 use PHPUnit\Framework\TestCase;
 
@@ -57,5 +58,19 @@ final class MonthTest extends TestCase
     {
         $this->assertEquals('2020-11', (new Month(2020, 11))->__toString());
         $this->assertEquals('2019-01', (new Month(2019, 1))->__toString());
+    }
+
+    public function testByString(): void
+    {
+        $this->assertEquals('2012-01', Month::byString('2012-01')->__toString());
+        $this->assertEquals('2012-01', Month::byString('2012-1')->__toString());
+    }
+
+    public function testByStringErrors(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectErrorMessage('Invalid string month provided, expected YYYY-MM');
+
+        Month::byString('foobar');
     }
 }
